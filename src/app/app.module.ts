7fea1979a0d59e4e0d91dc8e9610b686f7f5fc88
Juan Rendon <KingQRool>
2023-02-16 12:12:43 -0500

    Routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule  } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoanTableComponent } from './loan-table/loan-table.component';
+import { LoanFormComponent } from './loan-form/loan-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { CustomersModule } from './customers/customers.module';
+import { FormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoanTableComponent,
+    LoanFormComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    CustomersModule,
     FormsModule
   ],
   providers: [],
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers.component';
-
-
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    CustomersComponent
+    CustomersComponent,
+    CreateCustomerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports:[
+    CustomersComponent,
   ]
 })
 export class CustomersModule { }
