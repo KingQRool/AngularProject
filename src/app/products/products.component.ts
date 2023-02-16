fe030ca1162098e02a315433d496f832cc14289b
Santiago <santiagomonroy11@gmail.com>
2023-02-16 16:01:00 -0500

    New fixes in restructuration task.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,20 +7,4 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
-<<<<<<< HEAD:src/app/products/products/products.component.ts
-  register = {
-    productName: '',
-    productValue: ''
-  }
-  onRegister() {
-    console.log(this.register)
-  }
-
-  public showForm: boolean = false;
-  openForm() {
-    this.showForm = !this.showForm
-  }
-
-=======
->>>>>>> feature/SMonroy-products:src/app/products/products.component.ts
 }
