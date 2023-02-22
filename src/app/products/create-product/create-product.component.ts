876b61d1fc103651bd0973bf7cdf01da3b00212a
Santiago <santiagomonroy11@gmail.com>
2023-02-21 20:13:37 -0500

    Validations and some changes in styles

--- src/app/products/create-product/create-product.component.ts
+++ src/app/products/create-product/create-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Product } from 'src/app/models/product.model';
 
 @Component({
@@ -8,28 +8,35 @@ import { Product } from 'src/app/models/product.model';
   styleUrls: ['./create-product.component.scss']
 })
 export class CreateProductComponent {
-@Input() createProduct : Array<Product>=[];
-@Output() sendProduct : EventEmitter<Product> = new EventEmitter();
+  @Input() createProduct: Array<Product> = [];
+  @Output() sendProduct: EventEmitter<Product> = new EventEmitter();
 
-  error : boolean = false;
+  resultado!: string;
+  error: boolean = false;
 
   productName = new FormControl<any>('', Validators.required);
 
   constructor(
-  ){}
+  ) { }
 
   onRegister() {
     console.log(this.productName.value)
   }
 
-  public showForm : boolean = false;
-  openForm(){
+  public showForm: boolean = false;
+  openForm() {
     this.showForm = !this.showForm
   }
 
-
-  showProducts(){
+  showProducts() {
     console.log(this.createProduct);
+  }
+
+  submit() {
+    if (this.productName.valid)
+      this.resultado = "Todos los datos son válidos";
+    else
+      this.resultado = "Hay datos inválidos en el formulario";
   }
 
   ngOnInit(): void {
@@ -37,11 +44,11 @@ export class CreateProductComponent {
   }
 
 
-  addProduct(){
+  addProduct() {
     if (this.productName.valid) {
       const product = {
-        id : 0,
-        productName : this.productName.value
+        id: 0,
+        productName: this.productName.value
       }
       this.sendProduct.emit(product)
     }
