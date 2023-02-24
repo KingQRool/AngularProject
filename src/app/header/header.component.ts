22f156c7812323b6946e66eaa2ef2aa407aadbb5
Santiago <santiagomonroy11@gmail.com>
2023-02-24 15:37:00 -0500

    New changes in styles. And tries of SCROLL navbar.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -31,6 +31,14 @@ export class HeaderComponent {
       path: '/Productos'
     }
   ]
+  onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar-inverse');
+    } else {
+      element.classList.remove('navbar-inverse');
+    }
+  }
 }
 
 
