bf163ae8e595fcacf09ebf8219d2651e9c2c6984
Juan Rendon <KingQRool>
2023-03-01 10:43:34 -0500

    Format documents

--- src/app/customers/create-customer/create-customer.component.ts
+++ src/app/customers/create-customer/create-customer.component.ts
@@ -10,88 +10,88 @@ import { Customers } from 'src/app/models/customer.model';
 
 export class CreateCustomerComponent {
 
-  @Input() createCustomer: Array<Customers>  = [];
+  @Input() createCustomer: Array<Customers> = [];
   @Output() sendCustomer: EventEmitter<Customers> = new EventEmitter();
 
-    customerName = new FormControl<any>('',[Validators.required, Validators.maxLength(20)],);
-    customerTelephone = new FormControl<any>('',Validators.required);
-    customerEmail = new FormControl<any>('',[Validators.required, Validators.email]);
+  customerName = new FormControl<any>('', [Validators.required, Validators.maxLength(20)],);
+  customerTelephone = new FormControl<any>('', Validators.required);
+  customerEmail = new FormControl<any>('', [Validators.required, Validators.email]);
 
 
-    constructor(){
+  constructor() {
 
-    }
+  }
 
-    get isNameFieldValid() { return this.customerName.touched && this.customerName.valid;}
+  get isNameFieldValid() { return this.customerName.touched && this.customerName.valid; }
 
-    get isNameFieldInvalid() { return this.customerName.touched && this.customerName.invalid;}
+  get isNameFieldInvalid() { return this.customerName.touched && this.customerName.invalid; }
 
-    onRegister(){
-      console.log(this.customerName.value),
+  onRegister() {
+    console.log(this.customerName.value),
       console.log(this.customerTelephone.value),
       console.log(this.customerEmail.value)
-    }
+  }
 
-    showCustomers(){
-      console.log(this.createCustomer)
-    }
+  showCustomers() {
+    console.log(this.createCustomer)
+  }
 
-    submit(){
+  submit() {
 
-    }
+  }
 
-    ngOnInit(): void {
-      this.showCustomers();
-    }
+  ngOnInit(): void {
+    this.showCustomers();
+  }
 
-    showCustomerFormValues(){
-      console.log(this.customerName?.value);
-      console.log(this.customerTelephone?.value)
-      console.log(this.customerEmail?.value)
+  showCustomerFormValues() {
+    console.log(this.customerName?.value);
+    console.log(this.customerTelephone?.value)
+    console.log(this.customerEmail?.value)
 
-    }
+  }
 
-    sendFormToShow(){
-      this.showCustomerFormValues();
-    }
+  sendFormToShow() {
+    this.showCustomerFormValues();
+  }
 
-    addCustomer(){
-      if (this.customerName.valid){
-        const customer = {
-          customerId:'0',
-          customerName: this.customerName.value,
-          customerTelephone: this.customerTelephone.value,
-          customerEmail: this.customerEmail.value
-        }
-        this.sendCustomer.emit(customer)
+  addCustomer() {
+    if (this.customerName.valid) {
+      const customer = {
+        customerId: '0',
+        customerName: this.customerName.value,
+        customerTelephone: this.customerTelephone.value,
+        customerEmail: this.customerEmail.value
       }
+      this.sendCustomer.emit(customer)
     }
+  }
 
-    // ------------------------------VALIDACIONES ANTERIORES------------------------------------\\
-    // buildCustomerForm(){
-      //   this.customerForm = this.customerFormBuilder.group({
-        //       customerName:['',[Validators.required, Validators.maxLength(20)]],
-        //       customerTelephone:[''],
-        //       customerEmail:['',[Validators.required, Validators.email]]
-        //   })
-        // }
-        //------------------------------------------------------------------------------------------\\
+  // ------------------------------VALIDACIONES ANTERIORES------------------------------------\\
+  // buildCustomerForm(){
+  //   this.customerForm = this.customerFormBuilder.group({
+  //       customerName:['',[Validators.required, Validators.maxLength(20)]],
+  //       customerTelephone:[''],
+  //       customerEmail:['',[Validators.required, Validators.email]]
+  //   })
+  // }
+  //------------------------------------------------------------------------------------------\\
 
-        // ------------------------------MOSTRAR EN CONSOLA-----------------------------------------\\
-        // get customerName(){ return this.customerForm.get('customerName')}
-        // get customerTelephone(){ return this.customerForm.get('customerTelephone')}
-        // get customerEmail(){ return this.customerForm.get('customerEmail')}
-        // get customerDocumentType(){return this.customerForm.get('customerDocumentType')}
-        // get customerDocumentNumber(){ return this.customerForm.get('customerDocumentNumber')}
-        // get customerPassword(){ return this.customerForm.get('customerPassword')}
-        // ------------------------------------------------------------------------------------------\\
+  // ------------------------------MOSTRAR EN CONSOLA-----------------------------------------\\
+  // get customerName(){ return this.customerForm.get('customerName')}
+  // get customerTelephone(){ return this.customerForm.get('customerTelephone')}
+  // get customerEmail(){ return this.customerForm.get('customerEmail')}
+  // get customerDocumentType(){return this.customerForm.get('customerDocumentType')}
+  // get customerDocumentNumber(){ return this.customerForm.get('customerDocumentNumber')}
+  // get customerPassword(){ return this.customerForm.get('customerPassword')}
+  // ------------------------------------------------------------------------------------------\\
 
-        //---------------CONSTRUCTOR ANTERIOR-----------------\\
-            // constructor(
-            //   private customerFormBuilder:FormBuilder
-            // ){
-            //     this.buildCustomerForm();
-            // }
-        //----------------------------------------------------\\
+  //---------------CONSTRUCTOR ANTERIOR-----------------\\
+  // constructor(
+  //   private customerFormBuilder:FormBuilder
+  // ){
+  //     this.buildCustomerForm();
+  // }
+  //----------------------------------------------------\\
 
-      }
+}
